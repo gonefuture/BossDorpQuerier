819923ce25360aef9c6633ffc317b8c3ea2f6413
gonefuture <gonefuture@qq.com>
2020-03-30 14:35:59 +0800

    3/30

--- main.go
+++ main.go
@@ -1,45 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/Luxurioust/excelize"
 	"log"
-    "github.com/Luxurioust/excelize"
+	"strconv"
 )
 
-func main() {
-    excelName := "./ItemResource.xlsx"
-
-	f, err := excelize.OpenFile(excelName)
-    if err != nil {
-        log.Fatal(err)
-    }
- 
-    /*
-      //读取某个单元格的值
-        value, err := f.GetCellValue("成绩表", "D2")
-        if err != nil {
-            log.Fatal(err)
-        }
-        fmt.Println(value)
-    */
- 
-    // 读取某个表单的所有数据
-    sheetMap := f.GetSheetMap()
-
-    for 
-
-
-
-    fmt.Println(sheetMap)
-
-
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-    // for _, row := range rows {
-    //     for _, value := range row {
-    //         fmt.Printf("\t%s", value)
-    //     }
-    //     fmt.Println()
-    // }
+type ItemReward struct {
+	Amount int `json:"amount"`
+	Code int `json:"code"`
+	//Type int  `json:"type"`
 }
+
+type ItemResource struct {
+	Key int `int:"key"`
+	Name string `string:"name"`
+}
+
+
+func main() {
+
+	var data = `{"amount":1, "code":1, "type":"ITEM"}`
+
+	var itemReward ItemReward
+	if err := json.Unmarshal([]byte(data), &itemReward); err == nil {
+		fmt.Println("================json str 转struct==")
+		fmt.Println(itemReward)
+		fmt.Println(itemReward.Code)
+	}
+
+
+	EXCEL_FILE_NAME := "./ItemResource.xlsx"
+
+	f, err := excelize.OpenFile(EXCEL_FILE_NAME)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 读取某个表单的所有数据
+	sheetMap := f.GetSheetMap()
+
+	itemResourceStorage := make(map[int] ItemResource)
+
+	for _, sheetName := range sheetMap {
+       rows,err := f.GetRows(sheetName)
+       if err != nil {
+           log.Fatal(err)
+       }
+       for index, row := range rows {
+       		// 过滤标题栏
+		   if index <= 4 {
+			   continue
+		   }
+		   itemResource := ItemResource{}
+		   itemResource.Key, _ = strconv.Atoi(row[1])
+		   itemResource.Name = row[2]
+		   itemResourceStorage[itemResource.Key] = itemResource
+       }
+    }
+	for k,v := range itemResourceStorage{
+		println(k)
+		println(v.Name)
+	}
+
+	println(itemResourceStorage[itemReward.Code].Name)
+
+}
+
+
+
+
